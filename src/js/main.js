5af7bd17455d5eb8ce635f2c5f8f0d9dbcde05c6
Viktor Ponomarenko <ponomarenko.office@gmail.com>
2020-05-26 16:49:30 +0300

    add normalize

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-let manifest_css = './css/rev-css-manifest.json';
-console.log(manifest_css);
-console.log(manifest_css);
-console.log(manifest_css);
-console.log(manifest_css);
-console.log(manifest_css);
-console.log(manifest_css);
