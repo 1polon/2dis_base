81b1675bffc6baf9a6d3e6d083bdf44410ff16d2
Viktor Ponomarenko <ponomarenko.office@gmail.com>
2020-05-27 15:28:00 +0300

    add style fos index

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,52 @@
 
 
 // hover wrapper__item_inner
-let wrapper__item_inner = document.querySelector('.wrapper__item_inner');
-console.log(wrapper__item_inner);
+// let wrapper__item_inner = document.querySelector('.wrapper__item_inner');
+// console.log(wrapper__item_inner);
 
-[].forEach.call(wrapper__item_inner, function(el){
-    el.addEventListener('mousemove', console_aaa);
-    // el.style.color = 'orange';
-})
-
-
+// [].forEach.call(wrapper__item_inner, function(el){
+//     el.addEventListener('mousemove', console_aaa);
+//     // el.style.color = 'orange';
+// })
 
 
+// let x, y, x1, y1;
+// let wrapper = document.querySelector('body');
+// wrapper.addEventListener('mousemove', function( elem ){
+//     x = elem.clientX;
+//     y = elem.clientY;
+//     // console.log(x)
+//     // console.log(y)
+//     // console.log(x + 1)
+//     // console.log(y + 1)
 
-wrapper__item_inner.addEventListener('mousemove', function(elem) {
-        console.log(elem.clientX);
-        wrapper__item_inner.style.transform = String(
-            "perspective(100vh)"
-            + "rotateX(" + elem.pageY +"deg)"
-            + "rotateY(" + elem.pageX +"deg)"
-            
-            
-            ); 
-    })
+// })
+
+
+// wrapper__item_inner.addEventListener('mousemove', function(elem) {
+//         console.log(elem.clientX);
+//         wrapper__item_inner.style.transform = String("perspective(100vh)" + "rotateY(" + 180 +"deg)") + String( "rotateX(" + 180 +"deg)"); 
+//         wrapper__item_inner.style.background = 'black';
+//     });
+// wrapper__item_inner.addEventListener('mouseleave', function(elem) {
+//     console.log(elem.clientX);
+//     wrapper__item_inner.style.transform = String("perspective(100vh)" + "rotateY(" + 0 +"deg)"); 
+//     wrapper__item_inner.style.background = null;
+// });
+
     
-    function console_aaa(el) {
-        // el.style.color = 'orange';
-        console.log('rotate(' + el.pageX + 'deg)')
-}
+//     function console_aaa(el) {
+//         // el.style.color = 'orange';
+//         console.log('rotate(' + el.pageX + 'deg)')
+// }
+
+
+
+
+// let wrapper__item_inner_download = document.querySelector('.wrapper__item_inner_download');
+// [].forEach.call(wrapper__item_inner_download, function(el) {
+//     el.addEventListener('mousemove', function(el){
+//         wrapper__item_inner_download.style.height = '100%';
+
+//     })
+// })
