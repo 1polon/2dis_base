cb701735b3fbc8fb6dfddbdadd4305df2b81ce22
Viktor Ponomarenko <ponomarenko.office@gmail.com>
2020-05-27 14:25:41 +0300

    add rotate for fon listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,24 @@
 
 
 // hover wrapper__item_inner
-let wrapper__item_inner = document.getElementsByClassName('wrapper__item_inner');
+let wrapper__item_inner = document.querySelector('.wrapper__item_inner');
 console.log(wrapper__item_inner);
 
 [].forEach.call(wrapper__item_inner, function(el){
     el.addEventListener('mousemove', console_aaa);
+    // el.style.color = 'orange';
 })
 
-// wrapper__item_inner.addEventListener('mousemove', function(elem) {
-//     console.log(elem.clientX);
-// })
 
-function console_aaa(el) {
-    console.log(el.pageX)
+
+
+
+wrapper__item_inner.addEventListener('mousemove', function(elem) {
+        console.log(elem.clientX);
+        wrapper__item_inner.style.transform = String("rotate(" + elem.pageX +"deg)"); 
+    })
+    
+    function console_aaa(el) {
+        // el.style.color = 'orange';
+        console.log('rotate(' + el.pageX + 'deg)')
 }
