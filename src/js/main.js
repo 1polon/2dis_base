baecb9443526eca16b92c74b60ba2704ec47ee78
Viktor Ponomarenko <ponomarenko.office@gmail.com>
2020-05-27 14:05:10 +0300

    add listener with pageX

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
-console.log('asdf')
-console.log('asdf')
-console.log('asdf')
-console.log('asdf')
-console.log('asdf')
-console.log('asdf')
-console.log('asdf')
-// console.log('asdf') 
+
+
+// hover wrapper__item_inner
+let wrapper__item_inner = document.getElementsByClassName('wrapper__item_inner');
+console.log(wrapper__item_inner);
+
+[].forEach.call(wrapper__item_inner, function(el){
+    el.addEventListener('mousemove', console_aaa);
+})
+
+// wrapper__item_inner.addEventListener('mousemove', function(elem) {
+//     console.log(elem.clientX);
+// })
+
+function console_aaa(el) {
+    console.log(el.pageX)
+}
